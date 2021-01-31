d313fc75169ed8ccabf51bf1862c40cfebbe7c78
fcaldas <filipecss@gmail.com>
2021-01-31 12:41:33 +0000

    add greek modes

--- src/app/classes/music/scale.ts
+++ src/app/classes/music/scale.ts
@@ -4,14 +4,14 @@ import { Note, Interval } from './note';
 export class Intervals {
 
     interval: Array<Interval>;
-    
+
     constructor(list_of_intervals: Array<Interval>) {
         this.interval = list_of_intervals;
         let sum = 0;
-        for(let c of this.interval){
+        for (let c of this.interval) {
             sum += c;
         }
-        if(sum != 12){
+        if (sum != 12) {
             throw new Error("Scale does not end on root");
         }
 
@@ -20,46 +20,67 @@ export class Intervals {
 
 export class Scale {
 
-    starting_note:Note;
+    starting_note: Note;
     intervals: Intervals;
 
-    constructor(starting_note: Note, intervals: Intervals){
+    constructor(starting_note: Note, intervals: Intervals) {
         this.starting_note = starting_note;
-        this.intervals = intervals; 
+        this.intervals = intervals;
     }
 
-    getNotesInScale(): Note[]{
+    getNotesInScale(): Note[] {
         let notes = [this.starting_note];
         let cur_note = this.starting_note;
-        for(let i of this.intervals.interval){
+        for (let i of this.intervals.interval) {
             cur_note = cur_note.addInterval(i);
             notes.push(cur_note);
         }
         return notes;
     }
 
-    toString(){
-        let notes:Note[] = this.getNotesInScale();
+    toString() {
+        let notes: Note[] = this.getNotesInScale();
         let note_names: string[] = [];
-        for(let k of notes){
+        for (let k of notes) {
             note_names.push(k.toString());
         }
         return note_names.toString();
     }
 }
 
-var major_scale = new Intervals([Interval.tone, Interval.tone, Interval.semitone, 
-                                 Interval.tone, Interval.tone, Interval.tone, 
-                                 Interval.semitone]);
+var major_scale = new Intervals([Interval.tone, Interval.tone, Interval.semitone,
+Interval.tone, Interval.tone, Interval.tone,
+Interval.semitone]);
 
-var natural_minor_scale = new Intervals([Interval.tone, Interval.semitone, Interval.tone, 
-                                         Interval.tone, Interval.semitone, Interval.tone, 
-                                         Interval.tone]);
+var dorian_mode = new Intervals([Interval.tone, Interval.semitone,
+Interval.tone, Interval.tone, Interval.tone,
+Interval.semitone, Interval.tone])
+
+var phrygian_mode = new Intervals([Interval.semitone,
+Interval.tone, Interval.tone, Interval.tone,
+Interval.semitone, Interval.tone, Interval.tone])
+
+var lydian_mode = new Intervals([
+    Interval.tone, Interval.tone, Interval.tone,
+    Interval.semitone, Interval.tone, Interval.tone, Interval.semitone])
+
+var mixolydian_mode = new Intervals([
+    Interval.tone, Interval.tone,
+    Interval.semitone, Interval.tone, Interval.tone, Interval.semitone, Interval.tone])
+
+var natural_minor_scale = new Intervals([Interval.tone, Interval.semitone, Interval.tone, Interval.tone, Interval.semitone, Interval.tone, Interval.tone]);
+
+var locrian_mode = new Intervals([Interval.semitone, Interval.tone, Interval.tone, Interval.semitone, Interval.tone, Interval.tone, Interval.tone])
 
 var pentatonic_scale = new Intervals([Interval.tone, Interval.tone, Interval.augmented, Interval.tone, Interval.augmented]);
 
 export var ScaleTypes = {
-    "Major": major_scale,
-    "Natural Minor": natural_minor_scale,
+    "Major (Ionian)": major_scale,
+    "Dorian (D)": dorian_mode,
+    "Phrygian (E)": phrygian_mode,
+    "Lydian (F)": lydian_mode,
+    "Mixolydian (G)": mixolydian_mode,
+    "Natural Minor (Aeolian)": natural_minor_scale,
+    "Locrian (B)": locrian_mode,
     "Pentatonic": pentatonic_scale
 };
